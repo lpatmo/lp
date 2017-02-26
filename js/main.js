588ba80149d8d8a53600c96dc87b9cbab4b57708
lpatmo <linda.peng@alumni.duke.edu>
2017-02-26 09:17:43 -0800

    added link functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+$(function() {
+	$('#about').on('click', function() {
+		//console.log('clicked')
+		$('.about-more').fadeIn(500).removeClass('display-none').addClass('show');
+	});
+	$('html').on('click', function(e) {
+		//console.log('html')
+		if( !$(e.target).hasClass('show') && !$(e.target).hasClass('link') ) {
+			$('.show').fadeOut(500).removeClass('show').addClass('display-none');
+		}
+	});
+});
